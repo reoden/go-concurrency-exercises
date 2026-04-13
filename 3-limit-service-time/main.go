bc31e0a0e029e7fd9b99ae85b355b49284bc945b
reoden <reodenx@gmail.com>
2026-04-13 20:38:29 +0800

    [feature] implement request handling with time limit for non-premium users

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,8 @@
 
 package main
 
+import "time"
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +23,24 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	done := make(chan struct{})
+	tickCh := time.NewTimer(10 * time.Second)
+
+	go func() {
+		process()
+		done <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return true
+		case <-tickCh.C:
+			if !u.IsPremium {
+				return false
+			}
+		}
+	}
 }
 
 func main() {
